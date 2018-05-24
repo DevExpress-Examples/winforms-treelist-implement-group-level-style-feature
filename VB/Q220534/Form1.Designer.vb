43d8fdce78e1dc772b8bf000f7f0b2dfba2a4013
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-24 16:40:13 +0300

    initial commit

---
+++ VB/Q220534/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿Namespace Q220534
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.treeList = New DevExpress.XtraTreeList.TreeList()
+			Me.treeListColumn1 = New DevExpress.XtraTreeList.Columns.TreeListColumn()
+			CType(Me.treeList, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' treeList
+			' 
+			Me.treeList.Columns.AddRange(New DevExpress.XtraTreeList.Columns.TreeListColumn() { Me.treeListColumn1})
+			Me.treeList.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.treeList.Location = New System.Drawing.Point(0, 0)
+			Me.treeList.Name = "treeList"
+			Me.treeList.BeginUnboundLoad()
+			Me.treeList.AppendNode(New Object() { Nothing}, -1)
+			Me.treeList.AppendNode(New Object() { Nothing}, 0)
+			Me.treeList.AppendNode(New Object() { Nothing}, 1)
+			Me.treeList.AppendNode(New Object() { Nothing}, 1)
+			Me.treeList.AppendNode(New Object() { Nothing}, 0)
+			Me.treeList.AppendNode(New Object() { Nothing}, 4)
+			Me.treeList.AppendNode(New Object() { Nothing}, 4)
+			Me.treeList.AppendNode(New Object() { Nothing}, -1)
+			Me.treeList.AppendNode(New Object() { Nothing}, 7)
+			Me.treeList.AppendNode(New Object() { Nothing}, 8)
+			Me.treeList.AppendNode(New Object() { Nothing}, 8)
+			Me.treeList.AppendNode(New Object() { Nothing}, 7)
+			Me.treeList.AppendNode(New Object() { Nothing}, 11)
+			Me.treeList.AppendNode(New Object() { Nothing}, 11)
+			Me.treeList.EndUnboundLoad()
+			Me.treeList.Size = New System.Drawing.Size(399, 268)
+			Me.treeList.TabIndex = 0
+			Me.treeList.TreeLineStyle = DevExpress.XtraTreeList.LineStyle.None
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.treeList.NodeCellStyle += new DevExpress.XtraTreeList.GetCustomNodeCellStyleEventHandler(this.OnTreeListNodeCellStyle);
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.treeList.CustomDrawNodeIndent += new DevExpress.XtraTreeList.CustomDrawNodeIndentEventHandler(this.OnTreeListCustomDrawNodeIndent);
+			' 
+			' treeListColumn1
+			' 
+			Me.treeListColumn1.Caption = "treeListColumn1"
+			Me.treeListColumn1.FieldName = "treeListColumn1"
+			Me.treeListColumn1.MinWidth = 56
+			Me.treeListColumn1.Name = "treeListColumn1"
+			Me.treeListColumn1.Visible = True
+			Me.treeListColumn1.VisibleIndex = 0
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(399, 268)
+			Me.Controls.Add(Me.treeList)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.Load += new System.EventHandler(this.OnFormLoad);
+			CType(Me.treeList, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private WithEvents treeList As DevExpress.XtraTreeList.TreeList
+		Private treeListColumn1 As DevExpress.XtraTreeList.Columns.TreeListColumn
+	End Class
+End Namespace
+
---
+++ VB/Q220534/Form1.vb
@@ -0,0 +1,69 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraTreeList
+Imports DevExpress.XtraTreeList.ViewInfo
+Imports DevExpress.XtraTreeList.Nodes
+
+Namespace Q220534
+	Partial Public Class Form1
+		Inherits Form
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub OnTreeListCustomDrawNodeIndent(ByVal sender As Object, ByVal e As CustomDrawNodeIndentEventArgs) Handles treeList.CustomDrawNodeIndent
+			e.Handled = True
+			Dim tree As TreeList = DirectCast(sender, TreeList)
+			Dim groupLevelIndentRects As New Dictionary(Of Integer, Rectangle)()
+			CalcGroupLevelIndentRects(tree.ViewInfo, e.Node, e.Bounds, groupLevelIndentRects)
+			For Each kvp As KeyValuePair(Of Integer, Rectangle) In groupLevelIndentRects
+				e.Cache.FillRectangle(e.Cache.GetSolidBrush(GetColorByLevel(kvp.Key)), kvp.Value)
+			Next kvp
+			If e.Node.ParentNode Is Nothing Then
+				Dim linePen As Pen = e.Cache.GetPen(tree.ViewInfo.PaintAppearance.HorzLine.BackColor)
+				e.Cache.DrawLine(linePen, e.Bounds.Location, New Point(e.Bounds.Right, e.Bounds.Y))
+			End If
+		End Sub
+
+		Private Sub CalcGroupLevelIndentRects(ByVal viewInfo As TreeListViewInfo, ByVal currentNode As TreeListNode, ByVal bounds As Rectangle, ByVal rects As Dictionary(Of Integer, Rectangle))
+			If currentNode Is Nothing Then
+				Return
+			End If
+			If currentNode.HasChildren Then
+				rects.Add(currentNode.Level, New Rectangle(bounds.Right - viewInfo.RC.LevelWidth, bounds.Y, viewInfo.RC.LevelWidth, bounds.Height))
+			End If
+			bounds = New Rectangle(bounds.Location, New Size(bounds.Width - viewInfo.RC.LevelWidth, bounds.Height))
+			CalcGroupLevelIndentRects(viewInfo, currentNode.ParentNode, bounds, rects)
+		End Sub
+
+		Private Function GetColorByLevel(ByVal level As Integer) As Color
+			Select Case level
+				Case 0
+					Return Color.Red
+				Case 1
+					Return Color.Yellow
+				Case 2
+					Return Color.Green
+				Case Else
+					Return Color.Black
+			End Select
+		End Function
+
+		Private Sub OnTreeListNodeCellStyle(ByVal sender As Object, ByVal e As GetCustomNodeCellStyleEventArgs) Handles treeList.NodeCellStyle
+			If Not e.Node.HasChildren Then
+				Return
+			End If
+			e.Appearance.BackColor = GetColorByLevel(e.Node.Level)
+		End Sub
+
+		Private Sub OnFormLoad(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+			treeList.ExpandAll()
+		End Sub
+	End Class
+End Namespace
